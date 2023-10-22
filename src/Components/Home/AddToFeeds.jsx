9195be47b21d2824bdc0d1deb8238827ba861266
ahmedsamir-cs50 <ahmed.samir2051996@gmail.com>
2023-10-22 15:14:50 +0300

    merge

--- src/Components/Home/AddToFeeds.jsx
+++ src/Components/Home/AddToFeeds.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
-
+import add from '../../../public/images/banner-image.jpg'
 const AddToFeeds = () => {
   return (
-    <div>AddToFeeds</div>
+    <div className='add-to-feed'>
+      <img src={add}></img>
+    </div>
   )
 }
 
